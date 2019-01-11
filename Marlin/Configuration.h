21fbbb5913a40fe81e8705b4db4254736021fca7
Gino A Melone <gam_github@brikwarrior.com>
2019-01-10 23:44:00 -0500

    Updating for AM8 build

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -787,7 +787,7 @@
 
 // The size of the print bed
 #define X_BED_SIZE 218
-#define Y_BED_SIZE 214
+#define Y_BED_SIZE 212
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -35
@@ -795,7 +795,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 240
+#define Z_MAX_POS 220
 
 /**
  * Software Endstops
