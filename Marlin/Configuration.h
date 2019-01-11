f20c01abdd223a49710f939cfc0051c207a108f9
Gino A Melone <gam_github@brikwarrior.com>
2019-01-11 00:28:02 -0500

    Tweaking bed size for AM8

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -885,12 +885,12 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 218
-#define Y_BED_SIZE 212
+#define X_BED_SIZE 217
+#define Y_BED_SIZE 208
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -35
-#define Y_MIN_POS -8
+#define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
